e01a0d48cda6f55e847178345952c9b3535a1a90
tapas0206 <tapas.agrawal55@gmail.com>
2019-12-16 11:22:26 +0400

    Remove Comments

--- src/main/java/com/myservs/demo/ExcepHandlers/UserNotFoundException.java
+++ src/main/java/com/myservs/demo/ExcepHandlers/UserNotFoundException.java
@@ -8,7 +8,6 @@ public class UserNotFoundException extends RuntimeException {
 
 	public UserNotFoundException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 }
